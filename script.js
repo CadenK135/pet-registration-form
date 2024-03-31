cf1bdd5695e6a90dae2e7a0b161829f953cc350e
CadenK135 <ckolbly@student.cccd.edu>
2024-03-30 20:52:31 -0700

    Finish

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 function validateForm() {
   const form = document.forms['pet-info-form'];
   // TODO: Add checks here.
-  return true;
-}
+  
+    const bordetellaChecked = form['bordetella'].checked;
+    const dhlppChecked = form['dhlpp'].checked;
+    const rabiesChecked = form['rabies'].checked;
+  
+    const otherInfo = form['other-info-text-area'].value.trim();
+    console.log(bordetellaChecked, dhlppChecked, rabiesChecked, otherInfo)
+    if (!bordetellaChecked || !dhlppChecked || !rabiesChecked) {
+      if (otherInfo === '') {
+        alert('Please specify why all vaccinations are not selected in the text area below.');
+        return false; 
+      }
+    }
+  
+    return true;
+  }
+
+
